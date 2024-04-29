2328c183a56c0d2a416670409b95df3f8be0f20f
Calvin <24404089+plunG3@users.noreply.github.com>
2024-04-29 22:42:17 +0200

    modified initial replication, and some logging tweaks

--- ReplicateFolder.ps1
+++ ReplicateFolder.ps1
@@ -56,18 +56,19 @@ if (-not ($Source) -or -not ($Replica) -or -not ($LogPath)) {
 # Create log file if not already present
 if (-not (Test-Path -Path $LogPath)) {
     try { 
-        New-Item -Force -Path $LogPath -ItemType File | Out-Null 
+        New-Item -Force -Path $LogPath -ItemType File | Out-Null
+        Logging -Level "INFO" -LogPath $LogPath -Message "Successfully validated LogPath [$LogPath]."
     } catch { 
-        Write-Output "ERROR`tUnable to create log file in [$LogPath]." 
+        Write-Output "ERROR`tUnable to access or create log file in [$LogPath]. Exiting." 
         Exit 1
     }
-}
+} else { Logging -Level "INFO" -LogPath $LogPath -Message "Successfully validated LogPath [$LogPath]." }
 
 # Validate Source
 if (-not (Test-Path -Path $Source)) {
     Logging -Level "ERROR" -LogPath $LogPath -Message "Source path [$Source] is invalid! Exiting." 
     Exit 1
-}
+} else { Logging -Level "INFO" -LogPath $LogPath -Message "Successfully validated Source [$Source]." }
 
 # Validate Replica
 if (-not (Test-Path -Path $Replica)) {
@@ -75,120 +76,110 @@ if (-not (Test-Path -Path $Replica)) {
     try { 
         New-Item -Force -Path "$Replica\TouchTest" -ItemType File | Out-Null
         Remove-Item "$Replica\TouchTest"
-        Logging -Level "INFO" -LogPath $LogPath -Message "Successfully created and removed `"TouchTest`" in [$Replica]."
+        Logging -Level "INFO" -LogPath $LogPath -Message "Successfully validated Replica [$Replica]."
     } catch { 
         Logging -Level "ERROR" -LogPath $LogPath -Message "Replica path validation failed for [$Replica]." 
         Exit 1
     }
-}
+} else { Logging -Level "INFO" -LogPath $LogPath -Message "Successfully validated Replica [$Replica]." }
+
 
 # Start replication flow
 Logging -Level "INFO" -LogPath $LogPath -Message "Starting replication script!"
 Write-Output ""
 
-if (-not (Test-Path -Path $Replica)) {
-    Logging -Level "INFO" -LogPath $LogPath -Message "No Replica detected, copying Source entirely."
-    try { Copy-Item -Recurse $Source $Replica }
-    catch { 
-        Logging -Level "ERROR" -LogPath $LogPath -Message "Could not write to Replica path [$Replica]! Exiting."
-        Exit 1
-    }
-    Logging -Level "INFO" -LogPath $LogPath -Message "Replication completed!"
-    Exit 0
-} else {
-    Logging -Level "INFO" -LogPath $LogPath -Message "Existing Replica found, running diffcheck."
+# Get Source file list
+$SourceFiles = Get-ChildItem -File -Recurse $Source
 
-    # Get Source file list
-    $SourceFiles = Get-ChildItem -File -Recurse $Source
+# Get Replica file list
+$ReplicaFiles = Get-ChildItem -File -Recurse $Replica
 
-    # Get Replica file list
-    $ReplicaFiles = Get-ChildItem -File -Recurse $Replica
-
-
-    # Check for new files ADDED in Source
-    Write-Output ""
-    Logging -Level "INFO" -LogPath $LogPath -Message "Checking for new files in Source.."
-    $diffList = @()
-    # Check each file in Source exists in Replica
-    foreach ($Sourcefile in $SourceFiles) {
-        $found = $false
-        foreach ($ReplicaFile in $ReplicaFiles) {
-            if ($Sourcefile.fullName.Replace($Source, "") -eq $ReplicaFile.fullName.Replace($Replica, "")) {
-                $found = $true
-                break
-            }
-        }
-        if (!$found) {
-            $diffList += $Sourcefile
-        }
-    }
-    # Add files which didn't exist in Replica
-    if ($diffList.Count -gt 0) {
-        Logging -Level "INFO" -LogPath $LogPath -Message "Adding files to Replica:"
-        $diffList | ForEach-Object { Logging -Level "INFO" -LogPath $LogPath -Message $_.FullName }
-        $diffList | ForEach-Object { 
-            try   { Copy-Item $_.FullName $_.FullName.Replace($Source, $Replica) }
-            catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to write file to Replica!" }
-        }
-
-        # Update Replica file list
-        $ReplicaFiles = Get-ChildItem -File -Recurse $Replica
-    } else {
-        Logging -Level "INFO" -LogPath $LogPath -Message "No new files found."
-    }
-
-
-    # Check for existing files REMOVED from Source
-    Write-Output ""
-    Logging -Level "INFO" -LogPath $LogPath -Message "Checking for files removed from Source.."
-    $diffList = @()
-    # Check each file in Replica exists in Source
+# Check for new files ADDED in Source
+Logging -Level "INFO" -LogPath $LogPath -Message "Checking for new files in Source.."
+$diffList = @()
+# Check each file in Source exists in Replica
+foreach ($Sourcefile in $SourceFiles) {
+    $found = $false
     foreach ($ReplicaFile in $ReplicaFiles) {
-      $found = $false
-      foreach ($Sourcefile in $SourceFiles) {
-        if ($ReplicaFile.FullName.Replace($Replica, "") -eq $Sourcefile.FullName.Replace($Source, "")) {
-          $found = $true
-          break
+        if ($Sourcefile.fullName.Replace($Source, "") -eq $ReplicaFile.fullName.Replace($Replica, "")) {
+            $found = $true
+            break
         }
-      }
-      if (!$found) {
-        $diffList += $ReplicaFile
-      }
     }
-    # Remove files that only exist in Replica
-    if ($diffList.Count -gt 0) {
-        Logging -Level "WARN" -LogPath $LogPath -Message "Removing files from Replica:"
-        $diffList | ForEach-Object { Logging -Level "WARN" -LogPath $LogPath -Message $_.FullName }
-        $diffList | ForEach-Object { 
-            try   { Remove-Item $_.FullName }
-            catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to remove file from Replica!" }
-        }
+    if (!$found) {
+        $diffList += $Sourcefile
+    }
+}
+# Add files which didn't exist in Replica
+if ($diffList.Count -gt 0) {
+    Logging -Level "INFO" -LogPath $LogPath -Message "New files found, adding to Replica:"
+    $diffList | ForEach-Object { Logging -Level "INFO" -LogPath $LogPath -Message $_.FullName }
+    $diffList | ForEach-Object { 
+        try {
+            # New-Item cmdlet used to create new files in case they are nested in new folders
+            New-Item -ItemType File -Path $_.FullName.Replace($Source, $Replica) -Force | Out-Null
+            Copy-Item -Path $_.FullName -Destination $_.FullName.Replace($Source, $Replica) -Force 
+        } catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to write file to Replica!" }
+    }
 
-        # Update Replica file list
-        $ReplicaFiles = Get-ChildItem -File -Recurse $Replica
-        } else {
-        Logging -Level "INFO" -LogPath $LogPath -Message "No files removed."
-        }
+    # Update Replica file list
+    $ReplicaFiles = Get-ChildItem -File -Recurse $Replica
+} else {
+    Logging -Level "INFO" -LogPath $LogPath -Message "No new files found."
+}
 
-    # Check for file updates by comparing "LastWriteTime"
-    Write-Output ""
-    Logging -Level "INFO" -LogPath $LogPath -Message "Checking for file updates in Source.."
-    $SourceFiles | ForEach-Object {
-        $SourceFilePath = $_.FullName
-        $SourceFileUpdated = $_.LastWriteTime
-        $ReplicaFiles | ForEach-Object {
-            if ($_.FullName.Replace($Replica, "") -eq $SourceFilePath.Replace($Source, "")) {
-                $ReplicaFilePath = $_.FullName
-                $ReplicaFileUpdated = $_.LastWriteTime
-                if ($SourceFileUpdated -ne $ReplicaFileUpdated) {
-                    Logging -Level "INFO" -LogPath $LogPath -Message "Updating file: $SourceFilePath"
-                    try   { Copy-Item $SourceFilePath $SourceFilePath.Replace($Source, $Replica) }
-                    catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to update file in Replica!" }
-                }
+
+# Check for existing files REMOVED from Source
+Write-Output ""
+Logging -Level "INFO" -LogPath $LogPath -Message "Checking for files removed from Source.."
+$diffList = @()
+# Check each file in Replica exists in Source
+foreach ($ReplicaFile in $ReplicaFiles) {
+    $found = $false
+    foreach ($Sourcefile in $SourceFiles) {
+    if ($ReplicaFile.FullName.Replace($Replica, "") -eq $Sourcefile.FullName.Replace($Source, "")) {
+        $found = $true
+        break
+    }
+    }
+    if (!$found) {
+    $diffList += $ReplicaFile
+    }
+}
+# Remove files that only exist in Replica
+if ($diffList.Count -gt 0) {
+    Logging -Level "WARN" -LogPath $LogPath -Message "Found files to be removed from Replica:"
+    $diffList | ForEach-Object { Logging -Level "WARN" -LogPath $LogPath -Message $_.FullName }
+    $diffList | ForEach-Object { 
+        try   { Remove-Item $_.FullName }
+        catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to remove file from Replica!" }
+    }
+
+    # Update Replica file list
+    $ReplicaFiles = Get-ChildItem -File -Recurse $Replica
+    } else {
+    Logging -Level "INFO" -LogPath $LogPath -Message "No files removed."
+    }
+
+# Check for file updates by comparing "LastWriteTime"
+Write-Output ""
+Logging -Level "INFO" -LogPath $LogPath -Message "Checking for file updates in Source.."
+$SourceFiles | ForEach-Object {
+    $SourceFilePath = $_.FullName
+    $SourceFileUpdated = $_.LastWriteTime
+    $ReplicaFiles | ForEach-Object {
+        if ($_.FullName.Replace($Replica, "") -eq $SourceFilePath.Replace($Source, "")) {
+            $ReplicaFilePath = $_.FullName
+            $ReplicaFileUpdated = $_.LastWriteTime
+            if ($SourceFileUpdated -ne $ReplicaFileUpdated) {
+                Logging -Level "INFO" -LogPath $LogPath -Message "Updating: $SourceFilePath"
+                try   { Copy-Item $SourceFilePath $SourceFilePath.Replace($Source, $Replica) }
+                catch { Logging -Level "ERROR" -LogPath $LogPath -Message "Failed to update file in Replica!" }
             }
         }
     }
-
-    Logging -Level "INFO" -LogPath $LogPath -Message "Replication completed!"
-    Exit 0
 }
+
+Write-Output ""
+Logging -Level "INFO" -LogPath $LogPath -Message "Replication completed!"
+Exit 0
